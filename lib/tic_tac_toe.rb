5f9f739b6e84968a4947950d7a224f392b73fae5
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-16 00:35:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,41 +13,38 @@ class TicTacToe
     [2,4,6] # Right to Left diagonal
   ]
 
-  def display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-   
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(index, current_player)
+    @board[index] = current_player
+  end
+
+  def position_taken?(location)
+    @board[location] != " " && @board[location] != ""
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+  
 end
 
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
 
-def move(board, index, current_player)
-  board[index] = current_player
-end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
+
 
 def turn(board)
   puts "Please enter 1-9:"
